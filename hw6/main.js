fdb1191d74082fe714349eb77d6a331379c4de04
DanyloGlynjanyi <141626973+DanyloGlynjanyi@users.noreply.github.com>
2023-12-07 02:25:36 +0200

    Update main.js

--- hw6/main.js
+++ hw6/main.js
@@ -45,8 +45,8 @@ const sortNums = (nums, direction) => {
 };
 
 let nums = [11, 21, 3];
-console.log(sortNums(nums, 'ascending')); // [3, 11, 21]
-console.log(sortNums(nums, 'descending')); // [21, 11, 3]
+console.log(sortNums(nums, 'ascending')); 
+console.log(sortNums(nums, 'descending')); 
 
 // ==========================
 // - є масив
